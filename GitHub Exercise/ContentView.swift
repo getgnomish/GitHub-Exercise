d148267dc2dd70fee64571b86ae0bc041c08d712
Derek Meyers <getgnomish@gmail.com>
2024-09-27 20:58:02 -0400

    Version 1.0 - Red

--- GitHub Exercise/ContentView.swift
+++ GitHub Exercise/ContentView.swift
@@ -10,12 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "1.circle")
+                .resizable()
+                .scaledToFit()
+//                .foregroundStyle(.tint)
+            Text("Version 1.0")
+                .font(.largeTitle)
+                .fontWeight(.black)
         }
         .padding()
+                            
+        .foregroundStyle(.red)
+                            
     }
 }
 

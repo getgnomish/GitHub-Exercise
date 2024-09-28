badfb0b7406f11793779624436b222ffa291efc7
Derek Meyers <getgnomish@gmail.com>
2024-09-27 21:11:37 -0400

    Version 3.0 - Blue

--- GitHub Exercise/ContentView.swift
+++ GitHub Exercise/ContentView.swift
@@ -10,17 +10,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "2.circle")
+            Image(systemName: "3.circle")
                 .resizable()
                 .scaledToFit()
 //                .foregroundStyle(.tint)
-            Text("Version 2.0")
+            Text("Version 3.0")
                 .font(.largeTitle)
                 .fontWeight(.black)
         }
         .padding()
                             
-        .foregroundStyle(.green)
+        .foregroundStyle(.blue)
                             
     }
 }
